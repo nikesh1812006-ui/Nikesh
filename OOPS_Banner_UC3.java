0985cd6e3e926573d1a5ced2ead100231a3d2be3
nikesh1812006-ui <nikesh1812006@gmail.com>
2026-03-02 09:07:39 +0530

    UC3: Implemented String.join() for OOPS banner

---
+++ OOPS_Banner_UC3.java
@@ -0,0 +1,39 @@
+public class OOPS_Banner_UC3 {
+
+    public static void main(String[] args) {
+
+        // Letter O
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", " ***** "));
+
+        System.out.println(); // Space between letters
+
+        // Letter O
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", " ***** "));
+
+        System.out.println();
+
+        // Letter P
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*     *"));
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*      "));
+        System.out.println(String.join("", "*      "));
+
+        System.out.println();
+
+        // Letter S
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "*      "));
+        System.out.println(String.join("", " ***** "));
+        System.out.println(String.join("", "      *"));
+        System.out.println(String.join("", " ***** "));
+    }
+}
